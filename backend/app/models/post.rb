d7496d7f51e39b310bf5c79ffa6cef1619ec118d
kinnari <kinnari1022@yahoo.com>
2021-02-20 15:52:53 -0500

    refactored in index.js and created render funcion

--- backend/app/models/post.rb
+++ backend/app/models/post.rb
@@ -1,10 +1,10 @@
 class Post < ApplicationRecord
     belongs_to :cuisine
     scope :order_by_created_at, ->{order("created_at DESC")}
-    before_save :strftime
+   
           
-  def strftime
-    self.created_at = self.created_at.strftime("%B %e, %Y at %l:%M %p")
-  end
+    def created_at
+        attributes['created_at'].strftime("%B %e, %Y at %l:%M %p")
+      end
 
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do
-      #resources :posts
       resources :posts, only: [:create, :show, :index, :destroy]
-      #resources :users
       resources :cuisines
     end
   end
