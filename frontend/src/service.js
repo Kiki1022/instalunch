f5b884b05d9e2d8523707fe66a3cc1d46f595c10
kinnari <kinnari1022@yahoo.com>
2021-03-10 13:12:55 -0500

    removed comments

--- frontend/src/service.js
+++ frontend/src/service.js
@@ -1,23 +1,15 @@
 class ApiService {
-//JOB IS TO CONNECT W API, CREATE REQUEST, FETCHURL AND WAIT FOR IT TO COMPLETE.
-    constructor(BASE_URL){
 
-        this.BASE_URL = BASE_URL || `http://localhost:3000`
+    constructor(){
+
+        this.BASE_URL =  `http://localhost:3000`
 
     }
 
-    // static fetchCuisines(){
-    //     return fetch(`http://localhost:3000/api/v1/cuisines`)
-    //     .then(res => res.json())
-    //     .then(cuisines => cuisines.forEach(cuisine => {
-    //         console.log(cuisine)
-    //      })
-        
-    // }
     fetchPosts(){
-        return fetch(`${this.BASE_URL}/api/v1/posts`)//endpoint
-        .then(res => res.json())//CONVERTS RESPONSE TO JSON 
-        .then(res => res.data)//AND RETURNS DATA FROM THERE
+        return fetch(`${this.BASE_URL}/api/v1/posts`)
+        .then(res => res.json())
+        .then(res => res.data)
         }
 
     newPost(body){
@@ -25,7 +17,8 @@ class ApiService {
             method: "POST",
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify(body)
-        }).then(res => res.json())//returns a promise which resolves to the response object
+        }).then(res => res.json())
+       
     }
 
     deletePost(id){
